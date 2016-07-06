2c99fae10cbd2309f85d0b7051b25e61daa99293
mars5315 <374849519@qq.com>
2016-07-07 00:01:18 +0800

    本地修改上传

--- src/main/java/net/cloudsun/controller/HelloAction.java
+++ src/main/java/net/cloudsun/controller/HelloAction.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value="/test")
 public class HelloAction {
-	
+	/**
+	 * 这是范例
+	 * @return
+	 */
 	@RequestMapping(value="/hello",method=RequestMethod.GET)
 	public String hello(){
 		System.out.println("hello,Mr zhou!");
