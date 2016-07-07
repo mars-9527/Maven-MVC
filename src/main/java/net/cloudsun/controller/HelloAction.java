0f08753d308caf92d96435370302efb8204d0ff7
mars5315 <374849519@qq.com>
2016-07-07 09:43:11 +0800

    update

--- src/main/java/net/cloudsun/controller/HelloAction.java
+++ src/main/java/net/cloudsun/controller/HelloAction.java
@@ -10,10 +10,12 @@ public class HelloAction {
 	/**
 	 * 这是范例
 	 * @return
+	 * @author Lou Ge
 	 */
 	@RequestMapping(value="/hello",method=RequestMethod.GET)
 	public String hello(){
 		System.out.println("hello,Mr zhou!");
+		System.out.println("hello world!");
 		return "ok";
 	}
 
